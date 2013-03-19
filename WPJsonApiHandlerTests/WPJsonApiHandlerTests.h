baf152565a4ece671f8f748773c7c406b739c130
Tom Erik Støwer <testower@gmail.com>
2013-03-19 19:42:58 +0100

    First commit

---
+++ WPJsonApiHandlerTests/WPJsonApiHandlerTests.h
@@ -0,0 +1,13 @@
+//
+//  WPJsonApiHandlerTests.h
+//  WPJsonApiHandlerTests
+//
+//  Created by Tom Erik Støwer on 3/19/13.
+//  Copyright (c) 2013 Tom Erik Støwer. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WPJsonApiHandlerTests : SenTestCase
+
+@end
---
+++ WPJsonApiHandlerTests/WPJsonApiHandlerTests.m
@@ -0,0 +1,32 @@
+//
+//  WPJsonApiHandlerTests.m
+//  WPJsonApiHandlerTests
+//
+//  Created by Tom Erik Støwer on 3/19/13.
+//  Copyright (c) 2013 Tom Erik Støwer. All rights reserved.
+//
+
+#import "WPJsonApiHandlerTests.h"
+
+@implementation WPJsonApiHandlerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WPJsonApiHandlerTests");
+}
+
+@end
